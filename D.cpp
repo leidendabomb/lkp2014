11d653e3a197ab125f7238cba5b27e271d0be87b
T.A. de Jong <tadj42@gmail.com>
2014-10-16 15:21:53 +0200

    Los D slecht op

--- D.cpp
+++ D.cpp
@@ -1,3 +1,6 @@
+// LESSONS LEARNED:
+// - Off-by-one errors are off-by-one
+//
 #include <iostream>
 #include <vector>
 #include <set>
@@ -13,6 +16,8 @@ using namespace std;
 
 static unsigned int anger[1501];
 static unsigned int students[1501];
+static unsigned int angerBelow[1501];
+static unsigned int angerAbove[1501];
 
 int main()
 {
@@ -23,30 +28,41 @@ int main()
 	while (numEntries--)
 	{
 		memset(anger, 0, sizeof(anger));
+		memset(angerBelow, 0, sizeof(angerBelow));
 
+		unsigned int numStudents = 0;
 		int floors;
-		unsigned int angBelowSum = 0;
-		unsigned int angAboveSum = 0;
 
 		cin >> floors;
-		for (int i = 0; i < floors; i++)
+		for (int i = 1; i <= floors; i++)
 		{
 			cin >> students[i];
-			angAboveSum += students[i];
+			numStudents += students[i];
 		}
+
+		angerAbove[0] = numStudents;
+		angerBelow[floors] = students[floors]*floors;
+
+		for (int i = floors-1; i >= 0; i--)
+			angerBelow[i] = angerBelow[i+1] + students[i]*i;
 
 		for (int i = 1; i <= floors; i++)
 		{
 			anger[i] = 0xFFFFFFFF;
-			angBelowSum += anger[i-1];
-			angAboveSum -= anger[i];
+			angerAbove[i] = angerAbove[i-1] - students[i];
 
-			for (int j = 0; j < i; j++)
+			unsigned int angerBellow = 0;
+			for (int j = i-1; j >= 0; j--)
 			{
-				unsigned int ang = anger[j] + angBelowSum + angAboveSum;
+				unsigned int ang = anger[j] + angerAbove[i] + angerBellow;
+				/*ang += i*(angerAbove[j]-angerAbove[i]);
+				ang -= angerBelow[j]-angerBelow[i];*/
+				angerBellow += (i-j)*students[j];
 
 				if (ang < anger[i])
+				{
 					anger[i] = ang;
+				}
 			}
 		}
 
